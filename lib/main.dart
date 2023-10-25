3597a445dffc4786fcffadff6b6bc6814336f157
=Minnas <=phannhattrieu01234@gmail.com>
2023-10-26 03:20:37 +0700

    tutor card

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:lettutor/screen/Authentication/forgot_password.dart';
 import 'package:lettutor/screen/Authentication/login.dart';
 import 'package:lettutor/screen/Authentication/registration.dart';
+import 'package:lettutor/screen/tutors/widgets/tutor_card.dart';
 
 void main() {
   runApp(const MainApp());
@@ -15,7 +16,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: ForgotPasswordScreen(),
+          child: TutorSearchCard(),
         ),
       ),
     );
